ec08119cd55afbfeeae828b4ff06ad38035aecf8
Akshata Sangwai <akshatasangwai1234@gmail.com>
2021-12-27 08:59:57 +0530

    Feature: Implemented logging
    Added log4j dependency

---
+++ src/main/java/bootstrap/Driver.java
@@ -0,0 +1,48 @@
+package bootstrap;
+
+import java.util.*;
+
+import org.apache.log4j.DailyRollingFileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Priority;
+import org.apache.log4j.*;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import java.util.concurrent.*;
+
+
+public class Driver {
+    public static Logger logger = LoggerFactory.getLogger(Driver.class);
+    public static void main(String[] args) {
+        configureLogging("gitIntroLog","INFO");
+        System.out.println("Hello world");
+        logger.info("Program started");
+    }
+    public static String configureLogging(String logFile, String logLevel){
+        DailyRollingFileAppender dailyRollingFileAppender = new DailyRollingFileAppender();
+        switch (logLevel){
+            case "DEBUG":{
+                dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.DEBUG_INT));
+            }
+            case "WARN":{
+                dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.WARN_INT));
+            }
+            case "ERROR":{
+                dailyRollingFileAppender.setThreshold(Level.toLevel((Priority.ERROR_INT)));
+            }
+            default:{
+                dailyRollingFileAppender.setThreshold(Level.toLevel(Priority.INFO_INT));
+            }
+            break;
+        }
+
+        System.out.println("Log files written out at "+logFile);
+        dailyRollingFileAppender.setFile(logFile);
+        dailyRollingFileAppender.setLayout(new EnhancedPatternLayout("%d [%t] %-5p %c - %m%n"));
+
+        dailyRollingFileAppender.activateOptions();
+        org.apache.log4j.Logger.getRootLogger().addAppender(dailyRollingFileAppender);
+        return dailyRollingFileAppender.getFile();
+
+    }
+}
